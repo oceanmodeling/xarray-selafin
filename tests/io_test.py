55beefeba4bdcea7f499165490f2c6edd6ee61dc
Luc DURON <l.duron@cnr.tm.fr>
2025-05-28 14:17:26 +0200

    Avoid ResourceWarning (unclosed file)

--- tests/io_test.py
+++ tests/io_test.py
@@ -34,54 +34,61 @@ def write_netcdf(ds, nc_out):
     ds.to_netcdf(nc_out)
 
 
+def equals_dataset_vs_netcdf_export(ds, nc_out):
+    write_netcdf(ds, nc_out)
+    ds_nc = xr.open_dataset(nc_out)
+    return ds_nc.equals(ds)
+
+
 @TIDAL_FLATS
 def test_open_dataset(slf_in):
-    ds = xr.open_dataset(slf_in, engine="selafin")
-    assert isinstance(ds, xr.Dataset)
-    repr(ds)
+    with xr.open_dataset(slf_in, engine="selafin") as ds:
+        assert isinstance(ds, xr.Dataset)
+        repr(ds)
 
-    # Dimensions
-    assert ds.sizes["time"] == 17
-    assert ds.sizes["node"] == 648
-    if "r3d" in slf_in:
-        assert ds.sizes["plan"] == 21
+        # Dimensions
+        assert ds.sizes["time"] == 17
+        assert ds.sizes["node"] == 648
+        if "r3d" in slf_in:
+            assert ds.sizes["plan"] == 21
 
-    # Coordinates
-    assert "x" in ds.coords
-    assert "y" in ds.coords
-    assert "time" in ds.coords
+        # Coordinates
+        assert "x" in ds.coords
+        assert "y" in ds.coords
+        assert "time" in ds.coords
 
-    # Attributes
-    assert ds.attrs["endian"] == ">"
-    assert ds.attrs["date_start"] == (1900, 1, 1, 0, 0, 0)
-    assert "ipobo" in ds.attrs
-    assert "ikle2" in ds.attrs
-    if "r3d_" in slf_in:
-        assert "ikle3" in ds.attrs
-    else:
-        assert "ikle3" not in ds.attrs
+        # Attributes
+        assert ds.attrs["endian"] == ">"
+        assert ds.attrs["date_start"] == (1900, 1, 1, 0, 0, 0)
+        assert "ipobo" in ds.attrs
+        assert "ikle2" in ds.attrs
+        if "r3d_" in slf_in:
+            assert "ikle3" in ds.attrs
+        else:
+            assert "ikle3" not in ds.attrs
 
 
 @TIDAL_FLATS
 def test_to_netcdf(tmp_path, slf_in):
-    ds_slf = xr.open_dataset(slf_in, engine="selafin")
-    nc_out = tmp_path / "test.nc"
-    write_netcdf(ds_slf, nc_out)
-    ds_nc = xr.open_dataset(nc_out)
-    assert ds_nc.equals(ds_slf)
+    with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
+        nc_out = tmp_path / "test.nc"
+        write_netcdf(ds_slf, nc_out)
+        ds_nc = xr.open_dataset(nc_out)
+        assert ds_nc.equals(ds_slf)
 
 
 @TIDAL_FLATS
 def test_to_selafin(tmp_path, slf_in):
-    ds_slf = xr.open_dataset(slf_in, engine="selafin")
+    with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
 
-    # Remove some data which is rebuilt
-    del ds_slf.attrs["date_start"]
+        # Remove some data which is rebuilt
+        del ds_slf.attrs["date_start"]
 
-    slf_out = tmp_path / "test.slf"
-    ds_slf.selafin.write(slf_out)
-    ds_slf2 = xr.open_dataset(slf_out, engine="selafin")
-    assert ds_slf2.equals(ds_slf)
+        slf_out = tmp_path / "test.slf"
+        ds_slf.selafin.write(slf_out)
+
+        with xr.open_dataset(slf_out, engine="selafin") as ds_slf2:
+            assert ds_slf2.equals(ds_slf)
 
     # Compare binary files
     with open(slf_in, "rb") as in_slf1, open(slf_out, "rb") as in_slf2:
@@ -90,15 +97,16 @@ def test_to_selafin(tmp_path, slf_in):
 
 @TIDAL_FLATS
 def test_to_selafin_eager_mode(tmp_path, slf_in):
-    ds_slf = xr.open_dataset(slf_in, lazy_loading=False, engine="selafin")
+    with xr.open_dataset(slf_in, lazy_loading=False, engine="selafin") as ds_slf:
 
-    # Remove some data which is rebuilt
-    del ds_slf.attrs["date_start"]
+        # Remove some data which is rebuilt
+        del ds_slf.attrs["date_start"]
 
-    slf_out = tmp_path / "test.slf"
-    ds_slf.selafin.write(slf_out)
-    ds_slf2 = xr.open_dataset(slf_out, engine="selafin")
-    assert ds_slf2.equals(ds_slf)
+        slf_out = tmp_path / "test.slf"
+        ds_slf.selafin.write(slf_out)
+
+        with xr.open_dataset(slf_out, engine="selafin") as ds_slf2:
+            assert ds_slf2.equals(ds_slf)
 
     # Compare binary files
     with open(slf_in, "rb") as in_slf1, open(slf_out, "rb") as in_slf2:
@@ -108,34 +116,26 @@ def test_to_selafin_eager_mode(tmp_path, slf_in):
 @TIDAL_FLATS
 def test_slice(tmp_path, slf_in):
     # simple selection
-    ds_slf = xr.open_dataset(slf_in, engine="selafin")
-    nc_out = tmp_path / "test1.nc"
-    ds_slice = ds_slf.isel(time=0)
-    write_netcdf(ds_slice, nc_out)
-    ds_nc = xr.open_dataset(nc_out)
-    assert ds_nc.equals(ds_slice)
+    with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
+        nc_out = tmp_path / "test1.nc"
+        ds_slice = ds_slf.isel(time=0)
+        assert equals_dataset_vs_netcdf_export(ds_slice, nc_out)
     # simple range
-    ds_slf = xr.open_dataset(slf_in, engine="selafin")
-    nc_out = tmp_path / "test2.nc"
-    ds_slice = ds_slf.isel(time=slice(0, 10))
-    write_netcdf(ds_slice, nc_out)
-    ds_nc = xr.open_dataset(nc_out)
-    assert ds_nc.equals(ds_slice)
+    with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
+        nc_out = tmp_path / "test2.nc"
+        ds_slice = ds_slf.isel(time=slice(0, 10))
+        assert equals_dataset_vs_netcdf_export(ds_slice, nc_out)
     if "r3d" in slf_in:
         # multiple slices
-        ds_slf = xr.open_dataset(slf_in, engine="selafin")
-        nc_out = tmp_path / "test3.nc"
-        ds_slice = ds_slf.isel(time=slice(0, 10), plan=0)
-        write_netcdf(ds_slice, nc_out)
-        ds_nc = xr.open_dataset(nc_out)
-        assert ds_nc.equals(ds_slice)
+        with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
+            nc_out = tmp_path / "test3.nc"
+            ds_slice = ds_slf.isel(time=slice(0, 10), plan=0)
+            assert equals_dataset_vs_netcdf_export(ds_slice, nc_out)
         # multiple range slices
-        ds_slf = xr.open_dataset(slf_in, engine="selafin")
-        nc_out = tmp_path / "test4.nc"
-        ds_slice = ds_slf.isel(time=slice(0, 10), plan=slice(5, 10))
-        write_netcdf(ds_slice, nc_out)
-        ds_nc = xr.open_dataset(nc_out)
-        assert ds_nc.equals(ds_slice)
+        with xr.open_dataset(slf_in, engine="selafin") as ds_slf:
+            nc_out = tmp_path / "test4.nc"
+            ds_slice = ds_slf.isel(time=slice(0, 10), plan=slice(5, 10))
+            assert equals_dataset_vs_netcdf_export(ds_slice, nc_out)
 
 
 def test_from_scratch(tmp_path):
@@ -165,5 +165,5 @@ def test_from_scratch(tmp_path):
 
 @DIMS
 def test_dim(slf_in):
-    ds = xr.open_dataset(slf_in, engine="selafin")
-    repr(ds)
+    with xr.open_dataset(slf_in, engine="selafin") as ds:
+        repr(ds)
